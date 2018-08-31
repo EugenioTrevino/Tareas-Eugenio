74bf13fe66fb0778c86324f1dd5d8f796cbd671d
EugenioTrevino <42283097+EugenioTrevino@users.noreply.github.com>
2018-08-31 16:55:19 -0500

    Add files via upload

---
+++ TareaVelocimetro 2.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0
+    case VelocidadBaja = 25
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init (velocidadInicial : Velocidades){
+    self = velocidadInicial
+        
+    }
+    
+    
+}
+
+class Auto{
+    var velocidad : Velocidades
+    init(){
+        velocidad = Velocidades.Apagado
+    }
+    func cambioDeVelocidad() -> (actual : Int , velocidadEnCadena : String){
+        switch actual{
+        case (25 ,           
+        }
+            
+        }
+    }
+    
+    
+        
+    }
+    
+    
+    
+    
+    
+}
